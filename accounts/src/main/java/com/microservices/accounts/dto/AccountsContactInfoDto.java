1453e1a6986e1d4114c5182b52854724953617dd
Lakshmi Reddy <narasareddym999@gmail.com>
2023-12-02 16:59:33 +0530

    added configserver

--- accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
@@ -1,9 +1,18 @@
 package com.microservices.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.*;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
--- cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
@@ -1,9 +1,18 @@
 package com.microservices.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.*;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String,String> contactDetails,List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
