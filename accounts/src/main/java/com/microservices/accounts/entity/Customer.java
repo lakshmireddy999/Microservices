5210e61fe6d8034999cac4ece839362e6d71f03d
Lakshmi Reddy <narasareddym999@gmail.com>
2023-12-03 14:42:38 +0530

    mysql addistion

--- accounts/src/main/java/com/microservices/accounts/entity/Customer.java
+++ accounts/src/main/java/com/microservices/accounts/entity/Customer.java
@@ -1,19 +1,24 @@
 package com.microservices.accounts.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@Getter @Setter @ToString @AllArgsConstructor@NoArgsConstructor
-public class Customer extends BaseEntity {
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Customer extends  BaseEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name="customer_id")
     private Long customerId;
+
     private String name;
+
     private String email;
+
+    @Column(name="mobile_number")
     private String mobileNumber;
 
 }
